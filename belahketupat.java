903ac20b5800100e7a269e564f0ca0cb9059cb79
Yurvan Igo Wibowo <yurvan160@gmail.com>
2016-03-30 11:16:42 +0700

    Update belahketupat.java

--- belahketupat.java
+++ belahketupat.java
@@ -11,14 +11,14 @@ int tingkat = 10;
 int n = 12/2;
 for(int i=1;i<=n;i++){
 for(int k=n;k>i;k--){
-System.out.print(" ");
+System.out.print("0");
 }
 for(int j=1;j<=l;j++){
-System.out.print("+");
+System.out.print("*");
 }
 l+=2;
  
-System.out.println("");
+System.out.println("0");
 }
 l -= 4;
  
@@ -27,7 +27,7 @@ for(int k=i;k<=(n-1);k++){
 System.out.print(" ");
 }
 for(int j=l;j>=1;j--){
-System.out.print("+");
+System.out.print("*");
 }
 l-=2;
  
